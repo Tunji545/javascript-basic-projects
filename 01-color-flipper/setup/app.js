2586ccbb86156d4ed971d70bb6047417be894fd5
Afuwwu <86732511+Afuwwu@users.noreply.github.com>
2021-07-16 17:12:34 +0100

    Update app.js
    From Azeez Afuwwu

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,11 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025", "#006400", "#008000", "#008080"];
+const btn = document.getElementById("btn");
+const color = document.document.getElementByClassName(".color");
+btn.addEventListener("click", () => {
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+});
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
