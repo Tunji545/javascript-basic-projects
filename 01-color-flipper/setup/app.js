3379edfb8180df9785e627ac3f8a1ceeaa98a25d
Adejolaoluwa <84287904+JollyJola@users.noreply.github.com>
2021-07-16 09:46:50 +0000

    Update app.js
    I add the function that picks the colours randomly and the event listener changes the colour when the button is clicked.

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,13 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector("#color");
+
+function changeBg(){
+   return Math.floor(Math.random() * colors.length);
+}
+
+btn.addEventListener("click", ()=>{
+    const randomColor = changeBg();
+    document.body.style.backgroundColor = colors[randomColor];
+    color.textContent = colors[randomColor];
+})
